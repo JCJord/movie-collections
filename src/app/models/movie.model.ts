8a0208dd684e28c2400a7e3579191dc1fec8635a
jcjord <juliojc.jord@gmail.com>
2025-06-28 12:49:35 -0300

    refactor: rename SearchResponse to SearchMovieResponse and enhance FoundMovieResponse structure

--- src/app/models/movie.model.ts
+++ src/app/models/movie.model.ts
@@ -1,8 +1,16 @@
-export interface SearchResponse {
+export interface SearchMovieResponse {
   page: number;
   results: Show[];
   total_pages: number;
   total_results: number;
+}
+
+export interface FoundMovieResponse {
+  movie_results: Show[]
+  person_results: Show[]
+  tv_results: Show[]
+  tv_episode_results: Show[]
+  tv_season_results: Show[]
 }
 
 export interface Show {
@@ -20,4 +28,13 @@ export interface Show {
   video: boolean;
   vote_average: number;
   vote_count: number;
+}
+
+export interface ExternalId {
+  id: number
+  imdb_id: string
+  wikidata_id: string
+  facebook_id: any
+  instagram_id: any
+  twitter_id: any
 }
