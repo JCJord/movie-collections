ccec2a23bc90e1e9d62f237f3dba63de718ef4e4
jcjord <juliojc.jord@gmail.com>
2025-06-28 12:51:40 -0300

    feat: add MovieDetails module with routing and component declarations

---
+++ src/app/features/movie-details/movie-details.module.ts
@@ -0,0 +1,33 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MovieDetailsComponent } from './movie-details.component';
+import { MovieDialogComponent } from './components/movie-dialog/movie-dialog.component';
+import { MovieDetailsRoutingModule } from './movie-details.routing.module';
+import { MatDialogModule } from '@angular/material/dialog';
+import { ChartModule } from 'src/app/shared/chart/chart.module';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { StarRatingComponent } from './components/star-rating/star-rating.component';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+
+
+@NgModule({
+  declarations: [
+    MovieDetailsComponent,
+    MovieDialogComponent,
+    StarRatingComponent
+  ],
+  imports: [
+    CommonModule,
+    MovieDetailsRoutingModule,
+    MatDialogModule,
+    ChartModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatSnackBarModule
+  ]
+})
+export class MovieDetailsModule { }
