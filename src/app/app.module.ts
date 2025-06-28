d17c5b12de550a50e594a4e6dae7f25fe0350f9d
jcjord <juliojc.jord@gmail.com>
2025-06-27 22:50:06 -0300

    fix: add missing HttpClientModule import in app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,16 +6,18 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { CoreModule } from './core/core.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     CoreModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
