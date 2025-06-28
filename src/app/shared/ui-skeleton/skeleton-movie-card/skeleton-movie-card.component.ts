4267e4f93d91a96318cf6d9a751c2a9ca37f9b21
jcjord <juliojc.jord@gmail.com>
2025-06-27 22:51:39 -0300

    feat: add SkeletonMovieCardComponent with template, styles, and tests for loading state representation

---
+++ src/app/shared/ui-skeleton/skeleton-movie-card/skeleton-movie-card.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-skeleton-movie-card',
+  templateUrl: './skeleton-movie-card.component.html',
+  styleUrls: ['./skeleton-movie-card.component.scss']
+})
+export class SkeletonMovieCardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
