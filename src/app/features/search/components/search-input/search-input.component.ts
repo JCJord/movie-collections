2e0df1573389ce701ce69fc76543fb8e156d7fa7
jcjord <juliojc.jord@gmail.com>
2025-06-27 22:50:57 -0300

    feat: enhance search input component with improved layout and validation handling

--- src/app/features/search/components/search-input/search-input.component.ts
+++ src/app/features/search/components/search-input/search-input.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { getErrorMessage } from 'src/app/core/helpers/errorTranslateHelper';
 
@@ -8,21 +8,29 @@ import { getErrorMessage } from 'src/app/core/helpers/errorTranslateHelper';
   styleUrls: ['./search-input.component.scss']
 })
 export class SearchInputComponent implements OnInit {
+  @Output() onSearch = new EventEmitter<string>();
+
   searchForm = new FormGroup({
     search: new FormControl('', [
       Validators.required,
       Validators.minLength(3),
-      Validators.pattern('^[a-zA-Z0-9]*$')  // only alphanumeric
+      Validators.pattern('^[a-zA-Z0-9]*$')
     ])
   });
 
-  constructor() { }
-
   ngOnInit(): void {
-
   }
 
   getError(controlName: string): string | null {
     return getErrorMessage(this.searchForm.get(controlName));
   } 
+
+  search() {
+    if (this.searchForm.valid) {
+      const query = this.searchForm.get('search')?.value;
+      this.onSearch.emit(query);
+    } else {
+      this.searchForm.markAllAsTouched();
+    }
+  }
 }
