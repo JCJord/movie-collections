c2247a5681dddbd3a1524065c11c09b07e0a2207
jcjord <juliojc.jord@gmail.com>
2025-06-27 22:50:24 -0300

    fix: correct environment configuration for API key and URL

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,9 +3,10 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  tmdbApiKey: 'd95c537172c6071b79df6fef70c889db',
+  apiUrl: 'https://api.themoviedb.org/3/search/movie'
 };
-
 /*
  * For easier debugging in development mode, you can import the following file
  * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
